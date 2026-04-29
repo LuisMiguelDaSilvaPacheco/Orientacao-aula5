1e56bf2766a087509373c945c952a22e75a43c9a
Luís Miguel <lm.luismiguel10@gmail.com>
2026-04-29 13:57:59 -0300

    Implement constructor and user input for Retangulo
    Added constructor to Retangulo class and implemented user input for base and height.

--- atividade1-2.php
+++ atividade1-2.php
@@ -5,6 +5,11 @@ class Retangulo{
     public $base;
     public $altura;
 
+    function __construct($base, $altura){
+        $this->base = $this->base;
+        $this->altura = $this->altura;
+    }
+
     function area(){
         $area = $this->base * $this->altura;
 
@@ -15,4 +20,13 @@ class Retangulo{
 
         return $perimetro;
     }
+}
+for ($i=0; $i < 3; $i++) { 
+    $base = readline("Diga a base de um retângulo: ");
+    $altura = readline("Diga a altura de um retângulo: ");
+
+    $retangulo = new Retangulo($base, $altura);
+    $retangulo -> area();
+    $retangulo -> perimetro();
+    print "Este retângulo têm ". $retangulo->area(). " de área e ". $retangulo->perimetro(). " de perímetro\n\n";
 }
